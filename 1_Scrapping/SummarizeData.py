da31d95f54de3acba4b09addd9273c01d5720a3c
KKJSP <saurabh96jun@gmail.com>
2019-04-22 00:58:35 -0700

    Added SummarizeData.py

---
+++ 1_Scrapping/SummarizeData.py
@@ -0,0 +1,32 @@
+
+# Python libraries
+import csv
+from bs4 import BeautifulSoup
+
+# Custom script
+from ScrapeData import Scrape
+
+numPatents = 13539
+
+with open('patent_info.tsv', 'w+', newline = '') as tsvfile:
+	writer = csv.writer(tsvfile, delimiter = '\t')
+
+	header = [	"patID", 
+				"grantDate", 
+				"title",
+				"abstract",
+				"citations",
+				"CPC",
+				"inventor",
+				"assignee",
+				"claims",
+				"examiner",
+				"patClass",
+				"description"]
+
+	writer.writerow(header)
+
+	for i in range(1, numPatents):
+		with open('./ScrappedData/pat' + str(i) + '.html') as file:
+			data = Scrape(BeautifulSoup(file.read(), 'html.parser'))
+			writer.writerow(data)
